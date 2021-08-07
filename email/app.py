ad9a2a571a64c8a3768fbae68a6ff8eec00bb388
Brian Vermilion <brian.vermilion@gmail.com>
2021-08-07 10:12:34 -0700

    Added more logging to email lambda, email body and CL barrel level logging to understand odd case where well3 turned off from negative barrel value which should not be the case

--- email/app.py
+++ email/app.py
@@ -4,6 +4,13 @@ import os
 import re
 import json
 
+''' How to test/debug
+    1. Generate recent alert (needs to be same day?), by changing Alarm Low or High within the Sensaphone 'Configure Device' screen for a Zone/Sensor.
+    2. Look at AWS CloudWatch logs a grab the JSON with the 'messageId' and place in events test JSON files. Ex. ../events/email_test_well3.json
+    3. Make your code changes, if needed.
+    4. Make local build to test with, run:  sam build --use-container --template ../template.yaml
+    5. Run Lambda locally with test event:  sam local invoke email -e ../events/email_test_well3.json
+'''
 
 def lambda_handler(event, context):
     msg = []
@@ -28,19 +35,23 @@ def lambda_handler(event, context):
     else:
         body = parsed_msg.get_payload(decode=True)
 
+    # log email body
+    print(body.decode('UTF-8'))
+
     # parse body of email
     email_lines = body.splitlines()
     for idx, l in enumerate(email_lines):
         # from which Sentinel
         if 'From:' in str(l):
             sentinel = email_lines[idx + 1].decode('utf-8')
-            # Well#3 / TreatmentPlant
+            # Well#3 / TreatmentPlant etc
 
         # what alerted? looking for CL Barrel Low Alarm
         elif 'alarm' in str(l):
             alert_details = str(l).split('. ')
             cl_alert = re.search(r"Low level alarm .+ Chlorine Barrel Level", str(alert_details[0]), re.I)
             cl_level = re.search(r"\d+\.\d+", alert_details[2])[0]
+            print('cl_level= ', cl_level, 'float()', float(cl_level), 'float(cl_level) > 0', float(cl_level) > 0)
 
             # valid alert? if reading is > 0. If we lost power value will be negative.
             if cl_alert and float(cl_level) > 0:
@@ -59,10 +70,10 @@ def lambda_handler(event, context):
                 else:
                     msg = 'Unknown Sentinel, no mapping of pumps to turn off'
 
-    # This is to record what happens in CloudWatch logs. For some reason return value is not logged.
     if not msg:
-        msg = 'Email Alert did not match Low CL Barrel Alarm'
+        msg = 'Email Alert body does not match - no-op'
 
+    # This is to record what happens in CloudWatch logs. For some reason return value is not logged.
     print(json.dumps(msg))
     return msg
 
